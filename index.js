ce510cbb21ce8bd6e21a88da5b88e2a053162fb0
Jacekun <jaceorwell@gmail.com>
2020-09-20 12:16:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ client.on('message', message => {
 				if (chapterCount > 0)
 				{
 					// sort array
-					embedPages.sort(function(word.a, word.b){return word.a-word.b});
+					embedPages.sort(function(a, b){return a-b});
 					// send the message
 					var msg = "Results for search: >" + searchString + "<";
 					const FieldsEmbed = new Pagination.FieldsEmbed()
